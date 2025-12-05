de5a26b7c0f8eab4b6eb77d89749db7d8113a1a1
TOMASDIAS\tomas <tomasvieiradias@gmail.com>
2025-12-05 13:32:42 +0000

    Projeto Final: Feito

--- obj/Debug/net10.0/final.AssemblyInfo.cs
+++ obj/Debug/net10.0/final.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("final")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+93a8d6eb94e397cb20cb268f03b495b4b0a1c034")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+97a16dcedc66c6e814d1c17d77c2b9b9c66cbd34")]
 [assembly: System.Reflection.AssemblyProductAttribute("final")]
 [assembly: System.Reflection.AssemblyTitleAttribute("final")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
